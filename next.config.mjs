a56a05577e1fb517ff0d3cea2f097af877db7f8d
lefline <issamkharbache2@gmail.com>
2024-06-10 11:19:50 +0100

    disabling es lint for production

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default withNextIntl(nextConfig);
